6d55b9afe58a2deec8702182dae71e4d878b81d3
AishikDalui <aishikdalui@gmail.com>
2024-02-04 22:07:38 +0530

    https update

--- app4.js
+++ app4.js
@@ -1,4 +1,4 @@
-let api = 'http://www.omdbapi.com/?apikey=61e576a4&t='
+let api = 'https://www.omdbapi.com/?apikey=61e576a4&t='
 let title = document.getElementById('title');
 let date = document.getElementById('date');
 let plot = document.getElementById('plot');
